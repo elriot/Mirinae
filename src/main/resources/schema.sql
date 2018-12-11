9233f37800bf3a4616a8781c367dbfa8bf5b9955
mirinae <spkim8901@gmail.com>
2018-12-11 11:07:32 +0900

    first commit

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,14 +1,14 @@
-﻿create table if not exists movie (
-mv_num integer primary key,
-mv_title varchar(100),
-mv_rating varchar(20),
-mv_detail varchar(1000),
-mv_releasedate varchar(20),
-mv_postimage varchar(50),
-mv_trailer varchar(100),
-tt_num integer,
-mv_startdate varchar(20),
-mv_enddate varchar(20),
-mv_time varchar(20),
-mv_istrue varchar(10) default 'T'
+CREATE TABLE movie (
+  mv_num integer PRIMARY KEY,
+  mv_title varchar(100),
+  mv_rating varchar(20),
+  mv_detail varchar(1000),
+  mv_releaseDate varchar(20),
+  mv_postImage varchar(50),
+  mv_trailer varchar(100),
+  tt_num integer,
+  mv_startDate varchar(20),
+  mv_endDate varchar(20),
+  mv_time varchar(20),
+  mv_isTrue varchar(10) DEFAULT 'T'
 );
