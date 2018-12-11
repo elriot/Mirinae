1f5d197495ae6ffdefdb3c00a4c267a3444992f4
mirinae <spkim8901@gmail.com>
2018-12-11 11:46:42 +0900

    first commit

---
+++ src/main/resources/data.sql
@@ -0,0 +1,2 @@
+INSERT INTO movie (mv_title, mv_rating, mv_detail, mv_releaseDate, mv_postImage, mv_trailer, tt_num, mv_startDate, mv_endDate, mv_time, mv_isTrue) 
+VALUES ('아기고양이 쿠키의 모험', '전체이용가', '고양이이야기', '2018-11-30', 'cat', 'no', 1, '2018-11-30', '2018-12-02', '11:00', 'F');
